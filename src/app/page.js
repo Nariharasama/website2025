4813adcc60127bde734ce6e9b454598a624bed32
Furutsuki <hushengyuan030831@gmail.com>
2025-01-04 21:44:54 +0900

    basic animation2

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,8 @@
 "use client"
 import Image from "next/image";
-import {useState} from "react";
-import { motion } from "motion/react"
+import {useEffect, useState} from "react";
+import { motion, useAnimate,} from "motion/react"
+import { spring } from "motion"
 
 function Logo(){
     return (
@@ -21,10 +22,21 @@ function Logo(){
 }
 
 function Logos1(){
-    return (<div className={"w-full overflow-hidden relative h-[72px]"}>
-        <motion.div className={'absolute left-0'} animate={{opacity: [0, 0, 1]}} transition={{
+    const [scope,animate] = useAnimate()
+    const handleScroll = () => {
+        // 处理滚动事件的逻辑
+        animate(scope.current,{opacity: Math.max(0.4,1-0.6*(window.scrollY/720)),y:Math.max(-200,-window.scrollY*0.4)},{duration: 0})
+    };
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+    return (<div className={"w-full overflow-hidden relative h-[72px] overflow-visible"}>
+        <motion.div className={'absolute left-0'} initial={{opacity:0}} animate={{opacity: [0, 0, 1]}} transition={{
                 times: [0, 0.8, 1],
-                duration: 5,}}>
+                duration: 5,}} ref={scope}>
             <motion.div className={'absolute left-0'} animate={{x: [-936, 2808, -936, -936]}} transition={{
                 times: [0, 1, 1, 1],
                 duration: 10,
@@ -67,27 +79,49 @@ function Logos1(){
 }
 
 function MainInfo() {
+    const [scope1,animate1] = useAnimate()
+    const [scope2,animate2] = useAnimate()
+    const handleScroll = () => {
+        // 处理滚动事件的逻辑
+            animate1(scope1.current,{x: 1.5*(-window.scrollY)},{duration: 0})
+            animate2(scope2.current,{x: 1.5*(window.scrollY)},{duration: 0})
+    };
+    useEffect(() => {
+        animate1(scope1.current,{x: 0},{duration: 2,type:"tween",ease:"easeOut"})
+        animate2(scope2.current,{x: 0},{duration: 2,type:"tween",ease:"easeOut"})
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
     return (
         <div className={"w-full overflow-hidden flex h-[96px] align-middle items-center justify-center"}>
-            <motion.div animate={{x: [-1500, 0]}} transition={{
-                duration: 3,
-            }}>
+            <motion.div ref={scope1} initial={{x: -1500}}>
                 <text className={"font-space text-mid text-[72px] font-bold"}>Tech Design&nbsp;</text>
             </motion.div>
-            <motion.div animate={{x: [1500, 0]}} transition={{
-                duration: 3,
-            }}>
+            <motion.div initial={{x: 1500}}  ref={scope2}>
                 <text className={"font-space text-mid text-[72px] font-bold"}>for everyone's life.</text>
             </motion.div>
-
         </div>
+
     )
 
 }
 
 function Info2() {
+    const [scope,animate] = useAnimate()
+    const handleScroll = () => {
+        // 处理滚动事件的逻辑
+        animate(scope.current,{opacity: 1-(window.scrollY/720)},{duration: 0})
+    };
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
     return (
-        <div className={"w-full overflow-hidden flex h-[64px] align-middle items-center justify-center"}>
+        <div className={"w-full overflow-hidden flex h-[64px] align-middle items-center justify-center"} ref={scope}>
             <motion.div animate={{opacity: [0, 0, 1]}} transition={{
                 times: [0, 0.75, 1],
                 duration: 4,
@@ -114,11 +148,22 @@ function Tag(props) {
 
 
 function Tags() {
-    return (<div className={"w-full overflow-hidden relative h-[160px]"}>
-            <motion.div className={'absolute left-0'} animate={{opacity: [0, 0, 1]}} transition={{
+    const [scope,animate] = useAnimate()
+    const handleScroll = () => {
+        // 处理滚动事件的逻辑
+        animate(scope.current,{opacity: Math.max(0.2,1-0.6*(window.scrollY/360)),y:Math.min(400,window.scrollY*0.8)},{duration: 0})
+    };
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+    return (<div className={"w-full relative h-[160px] overflow-visible"}>
+            <motion.div className={'absolute left-0 overflow-visible'}  initial={{opacity:0}} animate={{opacity: [0, 0, 1]}} transition={{
                 times: [0, 0.8, 1],
                 duration: 5,
-            }}>
+            }} ref={scope}>
                 <motion.div className={'absolute left-0'} animate={{x: [-2106, 6318, 6318, -2106]}} transition={{
                     times: [0, 0, 0, 1],
                     duration: 30,
@@ -160,15 +205,98 @@ function Tags() {
         </div>
     )
 }
+function ScrollInfo(){
+    const [scope,animate] = useAnimate()
+    const handleScroll = () => {
+        // 处理滚动事件的逻辑
+        if(window.scrollY>160){
+            animate(scope.current,{opacity: 0},{duration: 0.2})
+        }else {
+            animate(scope.current,{opacity: 1},{duration: 0.2})
+        }
+        }
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+    return (
+        <div className={"w-full overflow-hidden flex h-[160px] align-middle items-center justify-center"} ref={scope}>
+            <motion.div animate={{opacity: [0, 0, 1]}} transition={{
+                times: [0, 0.75, 1],
+                duration: 6,
+            }} className={"w-full flex flex-col justify-center items-center"}>
+                <text className={"font-space text-dark text-[32px] font-regular"}>scroll down</text>
+                <Image src={"images/arrow_downward.svg"} alt={'down'} width={40} height={40}></Image>
+
+            </motion.div>
+        </div>
+    )
+
+
+}
 
 
 export default function Home() {
     return (
-        <div className={"h-dvh w-full justify-center flex flex-col align-middle items-center"}>
-            <Logos1/>
-            <MainInfo/>
-            <Info2/>
-            <Tags/>
+        <div className={'w-full flex flex-col'}>
+            <div className={"h-dvh w-full justify-center flex flex-col align-middle items-center fixed"}>
+                <div className={"w-full flex-grow"}></div>
+                <Logos1/>
+                <MainInfo/>
+                <Info2/>
+                <Tags/>
+                <div className={"w-full flex-grow"}></div>
+                <ScrollInfo/>
+            </div>
+            <div className={"h-dvh w-full justify-center flex flex-col align-middle items-center "}></div>
+            <div className={"h-[480px] w-full justify-center flex flex-col align-middle items-center "}>
+                <text className={"font-space text-mid text-[36px] font-bold"}>Hi there! Thanks for visiting my site.
+                </text>
+                <div className={"w-full h-[40px]"}></div>
+                <text className={"font-space text-dark text-[24px] font-regular"}>I’m a UI designer and doing front-end
+                    development at the same time.
+                </text>
+                <text className={"font-space text-dark text-[24px] font-regular"}>This site is completely designed,
+                    developed and deployed by myself.
+                </text>
+                <text className={"font-space text-dark text-[24px] font-regular"}>It’s mainly coded based on next.js with
+                    framer motion components.
+                </text>
+                <text className={"font-space text-dark text-[24px] font-regular"}>Sources can be found in my GitHub links
+                    at the bottom of the page
+                </text>
+            </div>
+            <div className={"h-[240px] w-full justify-center flex flex-col align-middle items-center "}/>
+            <div className={"h-[400px] w-full justify-center flex flex-col align-middle items-center "}>
+                <text className={"font-space text-mid text-[36px] font-bold"}>Let me introduce myself a bit.
+                </text>
+                <div className={"w-full h-[40px]"}></div>
+                <text className={"font-space text-dark text-[24px] font-regular"}>I was born in China and entered
+                    university at 15 via a special education project.
+                </text>
+                <text className={"font-space text-dark text-[24px] font-regular"}>-Xi’an Jiaotong University @Industrial
+                    Design-
+                </text>
+                <text className={"font-space text-dark text-[24px] font-regular"}>After finished my undergraduate degree
+                    I went Japan for further learning.
+                </text>
+                <text className={"font-space text-dark text-[24px] font-regular"}>-Chiba University @Design (Master)-
+                </text>
+                <text className={"font-space text-dark text-[24px] font-regular"}>And now I’m focusing on improving
+                    Japanese public transport guide app UIs.
+                </text>
+            </div>
+            <div className={"h-[560px] w-full justify-center flex flex-col align-middle items-center "}>
+                <text className={"font-space text-mid text-[36px] font-bold"}>Need some inspirations?
+                </text>
+                <div className={"w-full h-[40px]"}></div>
+                <text className={"font-space text-dark text-[36px] font-bold"}>Check my <text className={'text-mid cursor-pointer'}>Github</text> or <text className={'text-mid cursor-pointer'}>Projects</text>.
+                </text>
+            </div>
         </div>
+
     );
 }
